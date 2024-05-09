5792a26b488cb66ffd24cee5b963af1ac838d092
Luis_Eduardo <undernet1@gmail.com>
2024-05-08 22:48:32 -0400

    Added .gitignore file

--- PruebaTecnicaNetCore.App/Controllers/ArticleController.cs
+++ PruebaTecnicaNetCore.App/Controllers/ArticleController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using PruebaTecnicaNetCore.Models;
 using PruebaTecnicaNetCore.Models.DTOs;
 using PruebaTecnicaNetCore.Repository.Interfaces;
--- PruebaTecnicaNetCore.Models/obj/Debug/netcoreapp3.1/PruebaTecnicaNetCore.Models.AssemblyInfo.cs
+++ PruebaTecnicaNetCore.Models/obj/Debug/netcoreapp3.1/PruebaTecnicaNetCore.Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PruebaTecnicaNetCore.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c8e2122ee6ee2850bbabae76cc7e57c7aac0898")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebb57c99b39d0e90b93ea0171480d1e66cf3fb4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("PruebaTecnicaNetCore.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PruebaTecnicaNetCore.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
